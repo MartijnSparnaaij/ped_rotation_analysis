0d909161cdd5b16aeba0289ca3da9bcdc2373f6a
Martijn Sparnaaij <m.sparnaaij@tudelft.nl>
2021-12-20 15:08:11 +0100

    Added license info

--- shoulderGui.m
+++ shoulderGui.m
@@ -1,6 +1,24 @@
 % Graphical User Interface for annotating shoulders in video data
 %
-% © Martijn Sparnaaij (2019)
+% Copyright (c) 2021 Martijn Sparnaaij
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 
 function varargout = shoulderGui(action, varargin)
 
@@ -1718,7 +1736,7 @@ dataInd = appData.stepInd2dataInd(stepInd);
 appData.groups(groupInd).shoulders{dataInd,1} = (stepInd - 1)*appData.interval + 1;
 appData.groups(groupInd).shoulders{dataInd,2} = numel(points);
 
-pointData = nan(numel(points), 5);
+pointData = nan(numel(points), 4);
 
 for ii = 1:numel(points)
     userData = get(points(ii), 'UserData');
@@ -1727,7 +1745,8 @@ for ii = 1:numel(points)
     pointData(ii,3) = get(userData(1), 'XData');
     pointData(ii,4) = get(userData(1), 'YData');
     if ishghandle(userData(3))
-        pointData(ii,5) = double(get(userData(3), 'String')) - 64;
+        warning('Not supported')
+        %pointData(ii,5) = double(get(userData(3), 'String')) - 64;
     end
 end % for ii
 
@@ -1833,7 +1852,7 @@ for ii = 1:size(pointData,1)
     lnPpr.YData = pointData(ii,4);
     rightPoint = line(lnPpr);
     
-    if ~isnan(pointData(ii,5))
+    if size(pointData,2) == 5 && ~isnan(pointData(ii,5))
         shoulderTextPrp = appData.shoulderTextPrp;
         shoulderTextPrp.String = char(64 + pointData(ii,5));
         shoulderTextPrp.Position = [mean(pointData(ii,[1,3])), mean(pointData(ii,[2,4])), 0];
